1983aec497a67f2321a34c252c6cf2add5d469a2
Ichlas Wardy Gustama <cahyo4713@gmail.com>
2023-11-08 16:53:17 +0700

    Update quiz
    - Fix logic program
    - Added assertif typescript for problem solving filtering products by ID

--- quiz-1.ts
+++ quiz-1.ts
@@ -1,7 +1,8 @@
 type product = {
     id: number
     productName: string
-    category: category<number, string>
+    category: number | category<number, string>[] | category<number, string>,
+    quantity: number
 }
 
 type category<A, B> = {
@@ -15,7 +16,7 @@ type productCollectionType = {
 }
 
 interface AddProductFunctionType {
-    (idProduct?: number, productName?: string, category?: category<number, string>): string
+    (idProduct?: number, productName?: string, category?: number): string
 }
 
 interface AddCategoryFunctionType {
@@ -48,26 +49,36 @@ const productCollection: productCollectionType = {
     categories: []
 }
 
-const addProduct: AddProductFunctionType = (idProduct = 1, productName = 'Oreo', categoryData = {
-    id: 1,
-    categoryName: 'Makanan'
-}) => {
- productCollection.products.push({
-    id: idProduct,
-    productName: productName,
-    category: categoryData
- })
+const addProduct: AddProductFunctionType = (idProduct = 1, productName = 'Oreo', idCategory = 1, quantityProduct = 1) => {
+ const isCategoryAvailable = productCollection.categories.filter(value => value.id === idCategory)
 
- return 'New product created!'
+ if (isCategoryAvailable.length >= 1) {
+     productCollection.products.push({
+        id: idProduct,
+        productName: productName,
+        category: idCategory,
+        quantity: quantityProduct
+     })
+    
+     return 'New product created!'
+ } else {
+    return 'Category doesn\'t exist'
+ }
 }
 
 const addCategory: AddCategoryFunctionType = (idCategory = 1, categoryName = 'Makanan') => {
- productCollection.categories.push({
-    id: idCategory,
-    categoryName: categoryName
- })
+ const isCategoryAvailable = productCollection.categories.filter(value => value.id === idCategory || value.categoryName === categoryName)
 
- return 'New category created!'
+ if (isCategoryAvailable.length >= 1) {
+    return 'Category already exist'
+ } else {
+     productCollection.categories.push({
+        id: idCategory,
+        categoryName: categoryName
+     })
+
+     return 'New category created!'
+ }
 }
 
 const editProduct: EditProductFunctionType = (idProduct, productName) => {
@@ -85,11 +96,11 @@ const editProduct: EditProductFunctionType = (idProduct, productName) => {
         return result
     })
 
-    return filterProduct.length === 0 ? 'Product not found!' : updateProducts
+    return filterProduct.length <= 0 ? 'Product not found!' : updateProducts
 }
 
 const editCategory: EditCategoryFunctionType = (idCategory, categoryName) => {
-    const filterCategory:  category<number, string>[] = productCollection.categories.filter(value => value.id === idCategory)
+    const filterCategory: category<number, string>[] = productCollection.categories.filter(value => value.id === idCategory)
     const updateCategories: category<number, string>[] = productCollection.categories.map(value => {
         let result: category<number, string> = value
 
@@ -103,33 +114,42 @@ const editCategory: EditCategoryFunctionType = (idCategory, categoryName) => {
         return result
     })
 
-    return filterCategory.length === 0 ? 'Category not found!' : updateCategories
+    return filterCategory.length <= 0 ? 'Category not found!' : updateCategories
 }
 
 const showItem: ShowItemType = (type) => {
+    const showProducts: product[] = productCollection.products.map(value => {
+        return {
+            ...value,
+            category: productCollection.categories.filter(categoryValue => categoryValue.id === value.category)
+        }
+    })
+
     switch (type) {
         case ShowType.products:
-            return productCollection.products
+            return showProducts
         case ShowType.categories:
             return productCollection.categories
         default:
-            return productCollection.products
+            return showProducts
     }
 }
 
 const filterProductByIdCategory: FilterProductByIdCategoryType = (idCategory) => {
-    const filterData: product[] = productCollection.products.filter(value => value.category.id === idCategory)
+    const filterData: product[] = productCollection.products.filter(value => (
+        value.category === idCategory || (value.category as category<number, string>)?.id === idCategory
+    ))
 
-    return filterData.length === 0 ? 'Product by Category ID not found!' : filterData
+    return filterData.length <= 0 ? 'Product by Category ID not found!' : filterData
 }
 
-console.log('[*] Add Products 1: ', addProduct(), productCollection.products)
 console.log('[*] Add Categories 1: ', addCategory(), productCollection.categories)
-// console.log('[*] Add Products 2: ', addProduct(2, 'Taro', { id: 2, categoryName: 'Makanan' }), productCollection.products)
+console.log('[*] Add Products 1: ', addProduct(), productCollection.products)
 // console.log('[*] Add Categories 2: ', addCategory(2, 'Pakaian'), productCollection.categories)
-// console.log('[*] Add Products 3: ', addProduct(3, 'Biskuat', { id: 3, categoryName: 'Makanan' }), productCollection.products)
+// console.log('[*] Add Products 2: ', addProduct(2, 'Taro', 2), productCollection.products)
 // console.log('[*] Add Categories 3: ', addCategory(3, 'Elektronik'), productCollection.categories)
-// console.log('[*] Edit Products: ', editProduct(1, 'Biskuat'))
+// console.log('[*] Add Products 3: ', addProduct(3, 'Biskuat', 3), productCollection.products)
 // console.log('[*] Edit Categories: ', editCategory(1, 'Minuman'))
+// console.log('[*] Edit Products: ', editProduct(1, 'Biskuat'))
 // console.log('[*] Showing Products / Categories: ', showItem(ShowType.categories))
 // console.log('[*] Filtering Products by ID Category: ', filterProductByIdCategory(2))
